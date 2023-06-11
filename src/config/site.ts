cca020df789c1bf3658760b609242208966f81f9
eder3232 <eldebar.dk@gmaill.com>
2023-06-10 20:10:19 -0500

    let's start!

---
+++ src/config/site.ts
@@ -0,0 +1,27 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Next.js",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Blog",
+      href: "/",
+    },
+    {
+      title: "Programas",
+      href: "/",
+    },
+    //TODO - fixear la appbar, no es responsive
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
